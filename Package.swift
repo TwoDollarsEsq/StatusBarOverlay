42877f353675e401cf2754ab843eb6b46d76bc7d
Artyom Rudakov <two_dollars@icloud.com>
2020-01-09 12:03:08 +0200

    Improves Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,26 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
-    name: "StatusBarOverlay"
+    name: "StatusBarOverlay",
+    products: [
+        .library(
+            name: "StatusBarOverlay",
+            targets: ["StatusBarOverlay"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/ashleymills/Reachability.swift",
+            .upToNextMajor(from: "4.3.0")
+        )
+    ],
+    targets: [
+        .target(
+            name: "StatusBarOverlay",
+            dependencies: ["Reachability"],
+            path: "Sources"
+        )
+    ]
 )
